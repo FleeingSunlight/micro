53201d91fcbfa4e3ce2b8b025b2fb42139c41290
FleeingSunlight <hi@usagidon-h.dev>
2020-12-28 17:41:52 +0700

    feat(services/geometry): add initial gRPC server implementation

--- services/geometry/src/main.rs
+++ services/geometry/src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use tonic::{transport::Server, Request, Response, Status};
+use std::f64::consts::{PI}
+
+use geometry::geometry_service_servier::{GeometryService, GeometryServiceServer};
+use geometry::{AreaRes, AreaOfCircleReq, AreaOfRectangleReq};
+
+pub mod geometry {
+    tonic::include_proto!("geometry");
+}
+
+#[derive(Debug, Default)]
+pub struct GeometryService {}
+
+#[tonic::async_trait]
+impl GeometryServiceImpl for GeometryService {
+    async fn area_of_circle(
+        &self,
+        request: Request<AreaOfCircleReq>,
+    ) -> Result<Response<AreaRes>, Status> {
+        let area = geometry::AreaRes {
+            area: request.into_inner().radius * PI,
+        };
+
+        Ok(Response::new(area))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let geometryService = GeometryService::default();
+
+    Server::builder()
+        .add_service(GeometryServiceServer::new(geometryService))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
