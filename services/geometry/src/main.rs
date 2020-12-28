49ade79379d0565f7f061130878a43705fc7f411
FleeingSunlight <hi@usagidon-h.dev>
2020-12-28 18:31:53 +0700

    feat(services/geometry): add area_of_rectangle()

--- services/geometry/src/main.rs
+++ services/geometry/src/main.rs
@@ -1,7 +1,7 @@
 use tonic::{transport::Server, Request, Response, Status};
-use std::f64::consts::{PI}
+use std::f64::consts::{PI};
 
-use geometry::geometry_service_servier::{GeometryService, GeometryServiceServer};
+use geometry::geometry_service_server::{GeometryService, GeometryServiceServer};
 use geometry::{AreaRes, AreaOfCircleReq, AreaOfRectangleReq};
 
 pub mod geometry {
@@ -9,29 +9,38 @@ pub mod geometry {
 }
 
 #[derive(Debug, Default)]
-pub struct GeometryService {}
+pub struct GeometrySVC {}
 
 #[tonic::async_trait]
-impl GeometryServiceImpl for GeometryService {
+impl GeometryService for GeometrySVC {
     async fn area_of_circle(
         &self,
         request: Request<AreaOfCircleReq>,
     ) -> Result<Response<AreaRes>, Status> {
         let area = geometry::AreaRes {
-            area: request.into_inner().radius * PI,
+            area: request.into_inner().radius * (PI as f32),
         };
 
         Ok(Response::new(area))
+    }
+
+    async fn area_of_rectangle(
+        &self,
+        _request: Request<AreaOfRectangleReq>,
+    ) -> Result<Response<AreaRes>, Status> {
+        unimplemented!()
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse()?;
-    let geometryService = GeometryService::default();
+    let addr = "0.0.0.0:50051".parse()?;
+    let geometry_service = GeometrySVC::default();
+
+    println!("[services/geometry]: {}", addr);
 
     Server::builder()
-        .add_service(GeometryServiceServer::new(geometryService))
+        .add_service(GeometryServiceServer::new(geometry_service))
         .serve(addr)
         .await?;
 
